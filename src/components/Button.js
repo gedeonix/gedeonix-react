8f5f5f147d8d48b24bef8f5bf460c9a73de540b0
Przemysław Sołtan <przemyslaw.soltan@gmail.com>
2017-01-04 23:04:28 +0100

    refactoring demo in storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, {css} from "styled-components";
 
 const styleButton = (props) => css`
   background-color: ${props.background};
@@ -11,18 +11,18 @@ const styleButton = (props) => css`
 `;
 
 const fontSize = (props) => {
-  if(props.xs) return '0.6rem';
-  if(props.sm) return '0.8rem';
-  if(props.md) return '1.2rem';
-  if(props.lg) return '1.6rem';
+  if (props.xs) return '0.6rem';
+  if (props.sm) return '0.8rem';
+  if (props.md) return '1.2rem';
+  if (props.lg) return '1.6rem';
   return props.theme.button.fontSize;
 };
 
 const padding = (props) => {
-  if(props.xs) return '0.5em 1em';
-  if(props.sm) return '0.5em 1em';
-  if(props.md) return '0.45em 1em';
-  if(props.lg) return '0.4em 1em';
+  if (props.xs) return '0.5em 1em';
+  if (props.sm) return '0.5em 1em';
+  if (props.md) return '0.45em 1em';
+  if (props.lg) return '0.4em 1em';
   return props.theme.button.padding;
 };
 
@@ -36,32 +36,12 @@ const Button = styled.button`
   margin: 0.1em;
 `;
 
-export const DefaultButton = styled(Button)`
-  ${props => styleButton(props.theme.default)}
-`;
-
-export const PrimaryButton = styled(Button)`
-  ${props => styleButton(props.theme.primary)}
-`;
-
-export const SuccessButton = styled(Button)`
-  ${props => styleButton(props.theme.success)}
-`;
-
-export const WarningButton = styled(Button)`
-  ${props => styleButton(props.theme.warning)}
-`;
-
-export const DangerButton = styled(Button)`
-  ${props => styleButton(props.theme.danger)}
-`;
-
-export const LightButton = styled(Button)`
-  ${props => styleButton(props.theme.light)}
-`;
-
-export const DarkButton = styled(Button)`
-  ${props => styleButton(props.theme.dark)}
-`;
+export const DefaultButton = styled(Button)`${props => styleButton(props.theme.default)}`;
+export const PrimaryButton = styled(Button)`${props => styleButton(props.theme.primary)}`;
+export const SuccessButton = styled(Button)`${props => styleButton(props.theme.success)}`;
+export const WarningButton = styled(Button)`${props => styleButton(props.theme.warning)}`;
+export const DangerButton = styled(Button)`${props => styleButton(props.theme.danger)}`;
+export const LightButton = styled(Button)`${props => styleButton(props.theme.light)}`;
+export const DarkButton = styled(Button)`${props => styleButton(props.theme.dark)}`;
 
 export default DefaultButton;
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 
 import {ThemeProvider} from "styled-components";
-import theme from "../theme";
+import theme, { dark } from "../theme";
 
+import Body from "../components/Body";
 import Button, {PrimaryButton, SuccessButton, WarningButton, DangerButton, LightButton, DarkButton} from '../components/Button';
 
-storiesOf('Button', module)
-  .add('Colors', () => (
+storiesOf('Component', module)
+  .add('Button', () => (
     <ThemeProvider theme={theme}>
-      <div>
+      <Body>
+        <h1>Colors</h1>
         <Button onClick={action('clicked')}>Default</Button>
         <PrimaryButton onClick={action('clicked')}>Primary</PrimaryButton>
         <SuccessButton onClick={action('clicked')}>Success</SuccessButton>
@@ -17,18 +19,39 @@ storiesOf('Button', module)
         <DangerButton onClick={action('clicked')}>Danger</DangerButton>
         <LightButton onClick={action('clicked')}>Light</LightButton>
         <DarkButton onClick={action('clicked')}>Dark</DarkButton>
-      </div>
-    </ThemeProvider>
-  ))
-  .add('Size', () => (
-    <ThemeProvider theme={theme}>
-      <div>
+
+        <h1>Size</h1>
         <Button xs>Extra small</Button>
         <Button sm>Small</Button>
         <Button>Normal</Button>
         <Button md>Middle</Button>
         <Button lg>Large</Button>
-      </div>
+
+      </Body>
+    </ThemeProvider>
+  ))
+  .add('Input', () => (
+    <ThemeProvider theme={theme}>
+      <Body>
+        todo...
+      </Body>
+    </ThemeProvider>
+  ))
+;
+
+storiesOf('Theme', module)
+  .add('Light', () => (
+    <ThemeProvider theme={theme}>
+      <Body>
+        <DangerButton>Button</DangerButton>
+      </Body>
+    </ThemeProvider>
+  ))
+  .add('Dark', () => (
+    <ThemeProvider theme={dark}>
+      <Body>
+        <DangerButton>Button</DangerButton>
+      </Body>
     </ThemeProvider>
   ))
 ;
