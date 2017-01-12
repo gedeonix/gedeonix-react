26b6f0b2718be7e8d414eb776c90e6c1fce09a31
Przemysław Sołtan <przemyslaw.soltan@gmail.com>
2017-01-12 15:02:16 +0100

    configure index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+export AppBar from './components/AppBar';
+export Avatar from './components/Avatar';
+export Button from './components/Button';
+export Body from './components/Body';
+export Card from './components/Card';
+export Container from './components/Container';
+export Drawer from './components/Drawer';
+export Grid from './components/Grid';
+export Icon from './components/Icon';
+export Input from './components/Input';
+export Simple from './components/Simple';
+export Social from './components/Social';
+export Table from './components/Table';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export Theme from './Theme';
